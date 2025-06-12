239a22b3383034cd529a0b818d00f8746cea8e21
espiritusonico <espiritusonicoviajero@gmail.com>
2025-06-12 00:18:51 -0300

    Actualizar scripts.js

--- scripts.js
+++ scripts.js
@@ -1,16 +1,14 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const enterButton = document.getElementById("enterButton");
-  const welcome = document.getElementById("welcome");
-  const mainContent = document.getElementById("mainContent");
-  const menuButton = document.getElementById("menuButton");
-  const menu = document.getElementById("menu");
+const enterButton = document.getElementById("enterButton");
+const welcome = document.getElementById("welcome");
+const mainContent = document.getElementById("mainContent");
+const menuButton = document.getElementById("menuButton");
+const menu = document.getElementById("menu");
 
-  enterButton.addEventListener("click", function () {
-    welcome.classList.add("hidden");
-    mainContent.classList.remove("hidden");
-  });
+enterButton.addEventListener("click", function () {
+  welcome.classList.add("hidden");
+  mainContent.classList.remove("hidden");
+});
 
-  menuButton.addEventListener("click", () => {
-    menu.classList.toggle("menu-hidden");
-  });
+menuButton.addEventListener("click", () => {
+  menu.classList.toggle("menu-hidden");
 });
